a97c80c8b70b50a648daaefe43cc5149118c5035
Danish Farman <danish.farman2016@gmail.com>
2016-05-06 15:25:13 +0500

    initial project add

---
+++ app/src/main/java/de/bitshares_munich/smartcoinswallet/MainActivity.java
@@ -0,0 +1,17 @@
+package de.bitshares_munich.smartcoinswallet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getApplicationContext(), BalancesActivity.class);
+        startActivity(intent);
+    }
+}
