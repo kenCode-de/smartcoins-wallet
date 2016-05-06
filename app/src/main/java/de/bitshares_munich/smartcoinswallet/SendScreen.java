af17205cd24310c533916d71ba44cb58ec070c9e
Syed Muhammad Muzzammil <smuzzammil.knysys@gmail.com>
2016-05-06 18:36:18 +0500

    initial commit

---
+++ app/src/main/java/de/bitshares_munich/smartcoinswallet/SendScreen.java
@@ -0,0 +1,15 @@
+package de.bitshares_munich.smartcoinswallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by afnan on 5/6/16.
+ */
+public class SendScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.send_screen);
+    }
+}
