a6e9b153ad2c05176fcfecbd3388242de9505184
Qasim Abdani <qabdani.knysys@gmail.com>
2016-05-10 14:50:10 +0500

    Related to import back

---
+++ app/src/main/java/de/bitshares_munich/smartcoinswallet/ImportBackupActivity.java
@@ -0,0 +1,14 @@
+package de.bitshares_munich.smartcoinswallet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ImportBackupActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_backup);
+
+    }
+}
