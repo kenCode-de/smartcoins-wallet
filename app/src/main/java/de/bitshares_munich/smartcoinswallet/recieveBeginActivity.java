f574fb2adf2484ec1fa49eb2c31dcbb072b77816
Fawaz Ahmed <fahmed.knysys@gmail.com>
2016-05-16 15:50:09 +0500

    scwall commit

---
+++ app/src/main/java/de/bitshares_munich/smartcoinswallet/recieveBeginActivity.java
@@ -0,0 +1,13 @@
+package de.bitshares_munich.smartcoinswallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class recieveBeginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieve_begin);
+    }
+}
