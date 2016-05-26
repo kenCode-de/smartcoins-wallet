80685566d1dd13307b6663efb1268cbdd4053c1e
Syed Muhammad Muzzammil <smuzzammil.knysys@gmail.com>
2016-05-26 15:37:02 +0500

    contact screen , send intent

--- app/src/main/java/de/bitshares_munich/fragments/ContactsFragment.java
+++ app/src/main/java/de/bitshares_munich/fragments/ContactsFragment.java
@@ -2,6 +2,7 @@ package de.bitshares_munich.fragments;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -18,14 +19,28 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.bitshares_munich.Interfaces.BalancesDelegate;
+import de.bitshares_munich.Interfaces.ContactsDelegate;
+import de.bitshares_munich.smartcoinswallet.Add_Edit_Contacts;
 import de.bitshares_munich.smartcoinswallet.List_View_Activity;
 import de.bitshares_munich.smartcoinswallet.R;
+import de.bitshares_munich.smartcoinswallet.Share_Contact;
+import de.bitshares_munich.utils.Support_Methods;
 
 /**
  * Created by qasim on 5/10/16.
  */
-public class ContactsFragment extends Fragment {
+public class ContactsFragment extends Fragment implements ContactsDelegate {
+    public static ContactsDelegate contactsDelegate;
+
+    @Bind(R.id.contactslist)
+    ListView contactslist;
     List_View_Activity adapter;
+
+
     public ContactsFragment() {
         // Required empty public constructor
     }
@@ -33,7 +48,6 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -48,16 +62,11 @@ public class ContactsFragment extends Fragment {
 //        categories.add("Personal");
 //        categories.add("Travel");
         View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        ButterKnife.bind(this, view);
+        contactsDelegate=this;
 
-        //now you must initialize your list view
-        ListView listview =(ListView)view.findViewById(R.id.contactslist);
-
-        //EDITED Code
-       // ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.listview_contacts, items);
-
-
-        listview.setAdapter(new List_View_Activity(getActivity()));
-
+        adapter = new List_View_Activity(getActivity());
+        contactslist.setAdapter(adapter);
 //        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
 //                AlertDialog.Builder adb=new AlertDialog.Builder(getActivity());
@@ -79,7 +88,31 @@ public class ContactsFragment extends Fragment {
         return view;
 
     }
-
-
-
+    @OnClick(R.id.addcontact)
+    public void AddContact(){
+        Intent intent = new Intent(getActivity(), Add_Edit_Contacts.class);
+        intent.putExtra("activity",99999);
+        startActivity(intent);
+    }
+    @Override
+    public void OnUpdate(String s,int id){
+//        contactslist.destroyDrawingCache();
+//        contactslist.setVisibility(ListView.INVISIBLE);
+//        contactslist.setVisibility(ListView.VISIBLE);
+//        adapter.clear();
+      adapter.loadmore();
+//        adapter = new List_View_Activity(getActivity());
+//        adapter.clear();
+//// Add data to collection..
+//        collection.add();
+//// Refresh your listview..
+//        listview.getAdapter().notifyDataSetChanged();
+        adapter = new List_View_Activity(getActivity());
+      contactslist.setAdapter(adapter);
+    }
+    @OnClick(R.id.sharecontact)
+    public void ShareContact() {
+        Intent intent = new Intent(getActivity(), Share_Contact.class);
+        startActivity(intent);
+    }
 }
