9274e2abec4cbf28d81ed7742fb571e059c03f8b
Qasim Abdani <qabdani.knysys@gmail.com>
2016-06-22 11:50:10 +0500

    Related to  If the selected account is an LTM, then do not display “Upgrade to LTM” on homescreen, and, disable that upgrade button in the settings screen too by graying it out.

--- app/src/main/java/de/bitshares_munich/smartcoinswallet/SettingActivity.java
+++ app/src/main/java/de/bitshares_munich/smartcoinswallet/SettingActivity.java
@@ -141,13 +141,11 @@ public class SettingActivity extends BaseActivity {
         ButterKnife.bind(this);
         activity = this;
         progressDialog = new ProgressDialog(this);
-        btnUpgrade.setVisibility(View.GONE);
         accountDetails = tinyDB.getListObject(getString(R.string.pref_wallet_accounts), AccountDetails.class);
         init();
         populateDropDowns();
         tvAppVersion.setText("v" + BuildConfig.VERSION_NAME + getString(R.string.beta));
         updateBlockNumberHead();
-
 
     }
 
@@ -354,12 +352,13 @@ public class SettingActivity extends BaseActivity {
             }
             if (accountDetails.get(i).isLifeTime) {
                 ivLifeTime.setVisibility(View.VISIBLE);
-                btnUpgrade.setVisibility(View.GONE);
+                btnUpgrade.setEnabled(false);
+                btnUpgrade.setBackgroundColor(Color.rgb(211, 211, 211));
 
             } else {
                 ivLifeTime.setVisibility(View.GONE);
-                btnUpgrade.setVisibility(View.VISIBLE);
-
+                btnUpgrade.setEnabled(true);
+                btnUpgrade.setBackground(getResources().getDrawable(R.drawable.button_border));
             }
         }
 
@@ -442,11 +441,13 @@ public class SettingActivity extends BaseActivity {
                     brainKey = accountDetails.get(i).brain_key;
                     if (accountDetails.get(i).isLifeTime) {
                         ivLifeTime.setVisibility(View.VISIBLE);
-                        btnUpgrade.setVisibility(View.GONE);
+                        btnUpgrade.setEnabled(false);
+                        btnUpgrade.setBackgroundColor(Color.rgb(211, 211, 211));
 
                     } else {
                         ivLifeTime.setVisibility(View.GONE);
-                        btnUpgrade.setVisibility(View.VISIBLE);
+                        btnUpgrade.setEnabled(true);
+                        btnUpgrade.setBackground(getResources().getDrawable(R.drawable.button_border));
 
                     }
                 } else {
@@ -831,5 +832,21 @@ public class SettingActivity extends BaseActivity {
         }
 
         tinyDB.putListObject(getString(R.string.pref_wallet_accounts), accountDetails);
+    }
+
+    private void checkLifeTime() {
+        for (int i = 0; i < accountDetails.size(); i++) {
+            if (spAccounts.getSelectedItem().toString().equals(accountDetails.get(i).account_name)) {
+
+                if (accountDetails.get(i).isLifeTime) {
+                    btnUpgrade.setEnabled(true);
+                    btnUpgrade.setBackgroundColor(Color.rgb(211, 211, 211));
+                } else {
+                    btnUpgrade.setEnabled(false);
+                    btnUpgrade.setBackground(getResources().getDrawable(R.drawable.button_border));
+                    ;
+                }
+            }
+        }
     }
 }
