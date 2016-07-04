e9614391ed14fa6cf481a177cf0cd1bf73eb3350
Qasim Abdani <qabdani.knysys@gmail.com>
2016-07-04 16:10:58 +0500

    Related to Import Bin File duplicate account overwrite from webservice response.

--- app/src/main/java/de/bitshares_munich/utils/BinHelper.java
+++ app/src/main/java/de/bitshares_munich/utils/BinHelper.java
@@ -26,18 +26,15 @@ public class BinHelper {
         return b & 0xFF;
     }
 
-    public ArrayList<Integer> getBytesFromBinFile(String filePath)
-    {
-        try
-        {
+    public ArrayList<Integer> getBytesFromBinFile(String filePath) {
+        try {
             File file = new File(filePath);
             byte[] fileData = new byte[(int) file.length()];
             DataInputStream dis = new DataInputStream(new FileInputStream(file));
 
             ArrayList<Integer> result = new ArrayList<>();
 
-            for ( int i = 0 ; i < fileData.length ; i++ )
-            {
+            for (int i = 0; i < fileData.length; i++) {
                 int val = unsignedToBytes(dis.readByte());
 
                 //result += Integer.toString(val) + ",";
@@ -50,9 +47,7 @@ public class BinHelper {
 
             dis.close();
             return result;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return null;
         }
     }
@@ -63,7 +58,7 @@ public class BinHelper {
         ArrayList<AccountDetails> accountDetailsList = tinyDB.getListObject(context.getString(R.string.pref_wallet_accounts), AccountDetails.class);
         AccountDetails accountDetails = new AccountDetails();
         accountDetails.wif_key = accountDetail.wif_key;
-        accountDetails.pinCode=pinCode;
+        accountDetails.pinCode = pinCode;
         accountDetails.account_name = accountDetail.account_name;
         accountDetails.pub_key = accountDetail.pub_key;
         accountDetails.brain_key = brainKey;
@@ -73,15 +68,20 @@ public class BinHelper {
 
 
         for (int i = 0; i < accountDetailsList.size(); i++) {
-            accountDetailsList.get(i).isSelected = false;
+            if (accountDetailsList.get(i).account_name.equals(accountDetails.account_name)) {
+                accountDetailsList.remove(i);
+            }
         }
 
+        for (int i = 0; i < accountDetailsList.size(); i++) {
+            accountDetailsList.get(i).isSelected = false;
+        }
         accountDetailsList.add(accountDetails);
 
         tinyDB.putListObject(context.getString(R.string.pref_wallet_accounts), accountDetailsList);
 
         List<TransactionDetails> emptyTransactions = new ArrayList<>();
-        tinyDB.putTransactions( activity, context, context.getString(R.string.pref_local_transactions), new ArrayList<>(emptyTransactions) );
+        tinyDB.putTransactions(activity, context, context.getString(R.string.pref_local_transactions), new ArrayList<>(emptyTransactions));
 
         //Intent intent = new Intent(context, TabActivity.class);
         //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
