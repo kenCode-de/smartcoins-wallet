043679d217ef95e5d2615f91bf6806684d5ccd79
Yasir1brahim <yasir.ibrahim@gmail.com>
2016-08-05 18:17:43 +0500

    loading all decoded memos in one web call API integrated

---
+++ app/src/main/java/de/bitshares_munich/models/DecodeMemosArray.java
@@ -0,0 +1,13 @@
+package de.bitshares_munich.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mbilal on 5/24/16.
+ */
+public class DecodeMemosArray {
+    public String status;
+    public String msg;
+    public List<String> memos;
+}
