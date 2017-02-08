84aaa85d5da6c515100e7ba32595eb5969cd8c3d
Henry Varona <henry.varona@gmail.com>
2017-02-07 20:18:17 -0400

    -

--- app/src/main/java/de/bitsharesmunich/cryptocoincore/fragments/NoCurrencyAccountFragment.java
+++ app/src/main/java/de/bitsharesmunich/cryptocoincore/fragments/NoCurrencyAccountFragment.java
@@ -1,5 +1,9 @@
 package de.bitsharesmunich.cryptocoincore.fragments;
 
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -7,8 +11,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import de.bitshares_munich.smartcoinswallet.BackupBrainkeyActivity;
 import de.bitshares_munich.smartcoinswallet.R;
 import de.bitsharesmunich.cryptocoincore.adapters.ViewPagerAdapter;
 import de.bitsharesmunich.cryptocoincore.models.Coin;
@@ -57,7 +64,45 @@ public class NoCurrencyAccountFragment extends Fragment {
             public void onClick(View view) {
                 if (pager != null){
                     ((ViewPagerAdapter)pager.getAdapter()).changeBitcoinFragment();
-                    //pager.getAdapter().notifyDataSetChanged();
+
+                    final Dialog dialog = new Dialog(getContext() this, R.style.stylishDialog);
+                    dialog.setTitle(getString(R.string.backup_brainkey));
+                    dialog.setContentView(R.layout.activity_copybrainkey);
+                    final EditText etBrainKey = (EditText) dialog.findViewById(R.id.etBrainKey);
+                    try {
+                        String brainKey = getBrainKey();
+                        if (brainKey.isEmpty()) {
+                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.unable_to_load_brainkey), Toast.LENGTH_LONG).show();
+                            return;
+                        } else {
+                            etBrainKey.setText(brainKey);
+                        }
+                    } catch (Exception e) {
+
+                    }
+
+                    Button btnCancel = (Button) dialog.findViewById(R.id.btnCancel);
+                    btnCancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+                        }
+                    });
+                    Button btnCopy = (Button) dialog.findViewById(R.id.btnCopy);
+                    btnCopy.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(BackupBrainkeyActivity.this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
+                            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                            ClipData clip = ClipData.newPlainText("label", etBrainKey.getText().toString());
+                            clipboard.setPrimaryClip(clip);
+                            dialog.cancel();
+                        }
+                    });
+                    dialog.setCancelable(false);
+
+                    dialog.show();
+
                 }
 
             }
