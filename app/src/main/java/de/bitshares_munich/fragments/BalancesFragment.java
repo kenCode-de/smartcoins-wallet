kenCode-de/smartcoins-wallet

commit 65efaa932ee6d0bcda6f97bf03616f1b7024aeaa

Deciding the user's initial smartcoin based on a combination of locale and configuration information, also adjusting the fiat currency symbols

app/src/main/java/de/bitshares_munich/fragments/BalancesFragment.java
@@ -165,7 +165,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
     String to = "";
 
     String wifkey = "";
-    String finalFaitCurrency;
+//    String finalFaitCurrency;
 
     @Bind(R.id.load_more_values)
     Button loadMoreButton;
@@ -531,12 +531,13 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
                     Log.d(TAG, "assetsUpdater.onSuccess");
                     List<Asset> assets = (List<Asset>) response.result;
                     // Updating the database
-                    int count = database.putAssets(assets);
+                    database.putAssets(assets);
 
                     // Looking for smartcoin asset
                     for(Asset asset : assets){
                         if(asset.getObjectId().equals(mSmartcoin.getObjectId().toString())){
                             mSmartcoin = database.fillAssetDetails(asset);
+                            Log.d(TAG,"Setting smartcoin as: "+mSmartcoin.getSymbol());
                         }
                     }
 
@@ -545,6 +546,9 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
                     if(missingEquivalentValues != null){
                         processNextEquivalentValue();
                     }
+
+                    //TODO: Remove this from here and replace the balance update procedure with something better
+                    loadBalancesFromSharedPref();
                 }
             });
         }
@@ -812,23 +816,52 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             database.clearTransfers();
         }
 
+        // Getting the system's configuration locale
+        locale = getResources().getConfiguration().locale;
+
+        // Checking the app's configuration to override the system configuration
+        // country locale if it is other than an empty string.
+        String configurationCountry = Helper.fetchStringSharePref(getContext(), getString(R.string.pref_country), "");
+        if(!configurationCountry.equals("")){
+            locale = new Locale.Builder().setLocale(locale).setRegion(configurationCountry).build();
+        }
+
+        // Checking the app's configuration to override the system configuration
+        // language locale if it is other than an empty string.
+        language = Helper.fetchStringSharePref(getActivity(), getString(R.string.pref_language), "");
+        if(!language.equals("")){
+            locale = new Locale.Builder().setLocale(locale).setLanguage(language).build();
+        }
+
         /**
-         * Setting up the default country to be used in case the user hasn't set up the desired
-         * country in the user settings.
+         * Just checking if we still don't have a country setup in the locale, in which case
+         * we try the telephony manager and then if that too fails we just setup Germany as
+         * the default country.
          */
-        String defaultCountry = getResources().getConfiguration().locale.getCountry();
-        if(defaultCountry.equals("")){
+        String localeCountry = locale.getCountry();
+        if(localeCountry.equals("")){
+            Log.w(TAG, "Could not resolve country information, trying with the telephony manager");
             // If the locale mechanism fails to give us a country, we try
             // to get it from the TelephonyManager.
-            defaultCountry = Helper.getUserCountry(getContext());
-            if(defaultCountry.equals("")){
-                defaultCountry = Constants.DEFAULT_COUNTRY_CODE;
+            String telephonyCountry = Helper.getUserCountry(getContext());
+            if(telephonyCountry.equals("")){
+                Log.w(TAG,"Could not resolve country information again, falling back to the default");
+                telephonyCountry = Constants.DEFAULT_COUNTRY_CODE;
             }
+            locale = new Locale.Builder().setRegion(telephonyCountry).build();
         }
-        // Setting the "base" smartcoin for this user
-        String countryCode = Helper.fetchStringSharePref(getContext(), getString(R.string.pref_country), defaultCountry);
 
-        this.mSmartcoin = Smartcoins.getMap().get(countryCode.toUpperCase());
+        /**
+         * Just checking if we still don't have a language setup in the locale, in which
+         * case we fallback to english as the default.
+         */
+        String localeLanguage = locale.getLanguage();
+        if(localeLanguage.equals("")){
+            Log.w(TAG,"Could not resolve language information, falling back to english");
+            locale = new Locale.Builder().setLocale(locale).setLanguage(Constants.DEFAULT_LANGUAGE_CODE).build();
+        }
+
+        this.mSmartcoin = Smartcoins.getMap().get(locale.getCountry());
         HashMap<String, Asset> knownAssets = database.getAssetMap();
         if(!knownAssets.containsKey(this.mSmartcoin.getObjectId())){
             // If the smartcoin asset details are not known, we schedule an update from the full node.
@@ -836,9 +869,12 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             assetList.add(mSmartcoin);
             getMissingAssets = new WebsocketWorkerThread(new LookupAssetSymbols(assetList, mLookupAssetsSymbolsListener));
             getMissingAssets.start();
+            Log.d(TAG,"Don't know much about this smartcoin, making a network query");
         }else{
+            Log.d(TAG, "Already have the details in database, just filling them");
             mSmartcoin = database.fillAssetDetails(mSmartcoin);
         }
+        Log.d(TAG, String.format("Selected smartcoin: %s", mSmartcoin.getSymbol()));
     }
 
 
@@ -847,8 +883,6 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_balances, container, false);
         ButterKnife.bind(this, rootView);
-        language = Helper.fetchStringSharePref(getActivity(), getString(R.string.pref_language), "");
-        locale = new Locale(language);
         balanceActivity = getActivity();
         format = NumberFormat.getInstance(locale);
         tvUpgradeLtm.setPaintFlags(tvUpgradeLtm.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
@@ -991,16 +1025,16 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         isCheckedTimeZone = Helper.fetchBoolianSharePref(getActivity(), getString(R.string.pre_ischecked_timezone));
         Boolean accountNameChange = checkIfAccountNameChange();
 
-        if (accountNameChange || (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency)))
+        String smartcoinSymbol = mSmartcoin.getSymbol();
+        if (accountNameChange || (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(smartcoinSymbol)))
             llBalances.removeAllViews();
 
-        if (isHideDonationsChanged || accountNameChange || (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency))) {
-            if (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency)) {
+        if (isHideDonationsChanged || accountNameChange || (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(mSmartcoin.getSymbol()))) {
+            if (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(smartcoinSymbol)) {
                 loadBasic(true, accountNameChange, true);
             } else {
                 loadBasic(true, accountNameChange, false);
             }
-
         }
 
         if (!accountId.equals("")) {
@@ -1385,7 +1419,6 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         if (this.balanceItems.findBalanceItemBySymbol(assetName) != null){
             this.balanceItems.updateFaitBalanceItem(assetName, value);
         } else {
-            Log.i(TAG, "tvAsset tv Amount tvFaitAmount nulls");
             updateEquivalentAmount.postDelayed(getEquivalentCompRunnable, 500);
         }
     }
@@ -1398,22 +1431,15 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             }
         };
 
-        String faitCurrency = Helper.getFadeCurrency(getContext());
-
-        if (faitCurrency.isEmpty()) {
-            faitCurrency = "EUR";
-        }
-        finalFaitCurrency = faitCurrency;
-
         HashMap<String, ArrayList<String>> currenciesChange = new HashMap();
 
         for (int i = 0; i < accountAssets.size(); i++) {
             AccountAssets accountAsset = accountAssets.get(i);
-            if (!accountAsset.symbol.equals(faitCurrency)) {
+            if (!accountAsset.symbol.equals(mSmartcoin.getSymbol())) {
                 if (!currenciesChange.containsKey(accountAsset.symbol)) {
                     currenciesChange.put(accountAsset.symbol, new ArrayList());
                 }
-                currenciesChange.get(accountAsset.symbol).add(faitCurrency);
+                currenciesChange.get(accountAsset.symbol).add(mSmartcoin.getSymbol());
             }
         }
         this.getEquivalentComponent(currenciesChange, getEquivalentCompRunnable);
@@ -1735,8 +1761,6 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             Long newAmmount = Long.parseLong(newItem.getAmmount());
 
             if (oldAmmount > newAmmount) {
-                Log.d("Balances Update", "Balance sent");
-
                 ammountTextView.setTypeface(ammountTextView.getTypeface(), Typeface.BOLD);
                 ammountTextView.setTextColor(getResources().getColor(R.color.red));
 
@@ -1850,18 +1874,17 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             //Now, we update the fait (EquivalentComponent)
             if ((newAmmount != 0) && (!newItem.getFait().equals(""))) {
                 try {
-                    Log.d("Equivalent Value Update", "Changing Fait Text: "+newItem.getSymbol());
-                    final Currency currency = Currency.getInstance(finalFaitCurrency);
+                    final Currency currency = Currency.getInstance(mSmartcoin.getSymbol());
                     double d = convertLocalizeStringToDouble(returnFromPower(newItem.getPrecision(), newItem.getAmmount()));
                     final Double eqAmount = d * convertLocalizeStringToDouble(newItem.getFait());
-                    String faitString = "";
-                    if (Helper.isRTL(locale, currency.getSymbol())) {
-                        faitString = String.format(locale, "%.2f %s", eqAmount, currency.getSymbol());
-                    } else {
-                        faitString = String.format(locale, "%s %.2f", currency.getSymbol(), eqAmount);
-                    }
 
-                    faitTextView.setText(faitString);
+                    NumberFormat currencyFormatter = Helper.newCurrencyFormat(getContext(), currency, locale);
+                    Log.i(TAG, currencyFormatter.format(eqAmount));
+
+                    Log.i(TAG,String.format("smartcoin symbol: %s, fiat symbol: %s", mSmartcoin.getSymbol(), currency.getSymbol()));
+                    String fiatString = String.format(locale, "%s", currencyFormatter.format(eqAmount));
+
+                    faitTextView.setText(fiatString);
                     faitTextView.setVisibility(View.VISIBLE);
                 } catch (Exception e) {
                     Log.e(TAG, "Error in updateEquivalentValue : " + e.getMessage());
@@ -2269,7 +2292,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
     }
 
     void loadBasic(boolean onResume, boolean accountNameChanged, boolean faitCurrencyChanged) {
-
+        Log.d(TAG,"loadBasic");
         ArrayList<AccountDetails> accountDetails = tinyDB.getListObject(getString(R.string.pref_wallet_accounts), AccountDetails.class);
         if (accountDetails.size() == 1) {
             accountDetailsId = 0;
@@ -2562,7 +2585,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             }
 
         }else{
-            tableAdapter = new TransfersTableAdapter(getContext(), account, newData.toArray(new HistoricalTransferEntry[newData.size()]));
+            tableAdapter = new TransfersTableAdapter(getContext(), locale, account, newData.toArray(new HistoricalTransferEntry[newData.size()]));
             transfersView.setDataAdapter(tableAdapter);
         }
 

app/src/main/assets/correctedI18nCurrencySymbols.properties
@@ -1,0 +1,48 @@
+# Note that these are the currency symbols to use for the specified code when displaying in a DIFFERENT locale than
+# the home locale of the currency. This file can be edited as needed. In addition, if in some case one specific locale
+# would use a symbol DIFFERENT than the standard international one listed here, then an additional properties file
+# can be added making use of the standard ResourceBundle loading algorithm. For example, if we decided we wanted to
+# show US dollars as just $ instead of US$ when in the UK, we could create a file i18nCurrencySymbols_en_GB.properties
+# with the entry USD=$
+ARS=$AR
+AUD=AU$
+BOB=$b
+BRL=R$
+CAD=CAN$
+CLP=Ch$
+COP=COL$
+CRC=\u20A1
+HRK=kn
+CZK=K\u010D
+DOP=RD$
+XCD=EC$
+EUR=\u20AC
+GTQ=Q
+GYD=G$
+HNL=L
+HKD=HK$
+HUF=Ft
+INR=\u20B9
+IDR=Rp
+ILS=\u20AA
+JMD=J$
+JPY=JP\u00A5
+KRW=\u20A9
+NZD=NZ$
+NIO=C$
+PAB=B/.
+PYG=Gs
+PEN=S/.
+PHP=\u20B1
+PLN=\u007A\u0142
+RON=lei
+SGD=S$
+ZAR=R
+TWD=NT$
+THB=\u0E3F
+TTD=TT$
+GBP=\u00A3
+USD=$
+UYU=$U
+VEF=Bs
+VND=\u20AB

app/src/main/java/de/bitshares_munich/adapters/TransfersTableAdapter.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.Currency;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -17,8 +19,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import de.bitshares_munich.database.HistoricalTransferEntry;
-import de.bitshares_munich.models.Smartcoins;
-import de.bitshares_munich.smartcoinswallet.Constants;
 import de.bitshares_munich.smartcoinswallet.R;
 import de.bitshares_munich.utils.Helper;
 import de.bitsharesmunich.graphenej.AssetAmount;
@@ -35,10 +35,12 @@ public class TransfersTableAdapter extends TableDataAdapter<HistoricalTransferEn
     private String TAG = this.getClass().getName();
 
     private UserAccount userAccount;
+    private Locale locale;
 
-    public TransfersTableAdapter(Context context, UserAccount userAccount, HistoricalTransferEntry[] data) {
+    public TransfersTableAdapter(Context context, Locale locale, UserAccount userAccount, HistoricalTransferEntry[] data) {
         super(context, data);
         this.userAccount = userAccount;
+        this.locale = locale;
     }
 
     @Override
@@ -141,8 +143,8 @@ public class TransfersTableAdapter extends TableDataAdapter<HistoricalTransferEn
         TextView fiatAmountTextView = (TextView) root.findViewById(R.id.fiat_amount);
         AssetAmount smartcoinAmount = historicalTransfer.getEquivalentValue();
 
-        String language = Helper.fetchStringSharePref(getContext(), getContext().getString(R.string.pref_language), Constants.DEFAULT_LANGUAGE_CODE);
-        Locale locale = new Locale(language);
+//        String language = Helper.fetchStringSharePref(getContext(), getContext().getString(R.string.pref_language), Constants.DEFAULT_LANGUAGE_CODE);
+//        Locale locale = new Locale(language);
         String symbol = "";
         if(transferAmount.getAsset() != null){
             symbol = transferAmount.getAsset().getSymbol();
@@ -167,8 +169,13 @@ public class TransfersTableAdapter extends TableDataAdapter<HistoricalTransferEn
         }
 
         if(smartcoinAmount != null){
-            String fiatSymbol = Smartcoins.getFiatSymbol(smartcoinAmount.getAsset());
-            String eqValue = String.format("~ %s %.2f", fiatSymbol, Util.fromBase(smartcoinAmount));
+            Log.d(TAG,"Using smartcoin: "+smartcoinAmount.getAsset().getObjectId());
+            final Currency currency = Currency.getInstance(smartcoinAmount.getAsset().getSymbol());
+            NumberFormat currencyFormatter = Helper.newCurrencyFormat(getContext(), currency, locale);
+            String eqValue = currencyFormatter.format(Util.fromBase(smartcoinAmount));
+
+//            String fiatSymbol = Smartcoins.getFiatSymbol(smartcoinAmount.getAsset());
+//            String eqValue = String.format("~ %s %.2f", fiatSymbol, Util.fromBase(smartcoinAmount));
             fiatAmountTextView.setText(eqValue);
         }else{
             Log.w(TAG, String.format("Fiat amount is null for transfer: %d %s", transferAmount.getAmount().longValue(), transferAmount.getAsset().getSymbol()));

commit be540e5345168ddf415e3c3ebb08d7ddf5e83d11

Partial fix for bugs introduced by commit 65efaa932ee6d0bcda6f97bf03616f1b7024aeaa

app/fabric.properties
@@ -1,0 +1,5 @@
+#This is autogenerated fabric property from system environment to prevent key to be committed to source control.
+#Mon, 16 Jan 2017 12:13:25 -0500
+
+apiSecret=8984cfaa7abf34bba76eb8d5f36d86861a0ba20109cf3e740287561f8dde0a46
+apiKey=381b5bc1ec3efde35e127128fcf6a078bc126313

app/src/main/java/de/bitshares_munich/fragments/BalancesFragment.java
@@ -165,7 +165,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
     String to = "";
 
     String wifkey = "";
-//    String finalFaitCurrency;
+    String finalFaitCurrency;
 
     @Bind(R.id.load_more_values)
     Button loadMoreButton;
@@ -531,13 +531,12 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
                     Log.d(TAG, "assetsUpdater.onSuccess");
                     List<Asset> assets = (List<Asset>) response.result;
                     // Updating the database
-                    database.putAssets(assets);
+                    int count = database.putAssets(assets);
 
                     // Looking for smartcoin asset
                     for(Asset asset : assets){
                         if(asset.getObjectId().equals(mSmartcoin.getObjectId().toString())){
                             mSmartcoin = database.fillAssetDetails(asset);
-                            Log.d(TAG,"Setting smartcoin as: "+mSmartcoin.getSymbol());
                         }
                     }
 
@@ -546,9 +545,6 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
                     if(missingEquivalentValues != null){
                         processNextEquivalentValue();
                     }
-
-                    //TODO: Remove this from here and replace the balance update procedure with something better
-                    loadBalancesFromSharedPref();
                 }
             });
         }
@@ -816,52 +812,10 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             database.clearTransfers();
         }
 
-        // Getting the system's configuration locale
-        locale = getResources().getConfiguration().locale;
+        // Setting the "base" smartcoin for this user
+        String countryCode = Helper.fetchStringSharePref(getContext(), getString(R.string.pref_country));
 
-        // Checking the app's configuration to override the system configuration
-        // country locale if it is other than an empty string.
-        String configurationCountry = Helper.fetchStringSharePref(getContext(), getString(R.string.pref_country), "");
-        if(!configurationCountry.equals("")){
-            locale = new Locale.Builder().setLocale(locale).setRegion(configurationCountry).build();
-        }
-
-        // Checking the app's configuration to override the system configuration
-        // language locale if it is other than an empty string.
-        language = Helper.fetchStringSharePref(getActivity(), getString(R.string.pref_language), "");
-        if(!language.equals("")){
-            locale = new Locale.Builder().setLocale(locale).setLanguage(language).build();
-        }
-
-        /**
-         * Just checking if we still don't have a country setup in the locale, in which case
-         * we try the telephony manager and then if that too fails we just setup Germany as
-         * the default country.
-         */
-        String localeCountry = locale.getCountry();
-        if(localeCountry.equals("")){
-            Log.w(TAG, "Could not resolve country information, trying with the telephony manager");
-            // If the locale mechanism fails to give us a country, we try
-            // to get it from the TelephonyManager.
-            String telephonyCountry = Helper.getUserCountry(getContext());
-            if(telephonyCountry.equals("")){
-                Log.w(TAG,"Could not resolve country information again, falling back to the default");
-                telephonyCountry = Constants.DEFAULT_COUNTRY_CODE;
-            }
-            locale = new Locale.Builder().setRegion(telephonyCountry).build();
-        }
-
-        /**
-         * Just checking if we still don't have a language setup in the locale, in which
-         * case we fallback to english as the default.
-         */
-        String localeLanguage = locale.getLanguage();
-        if(localeLanguage.equals("")){
-            Log.w(TAG,"Could not resolve language information, falling back to english");
-            locale = new Locale.Builder().setLocale(locale).setLanguage(Constants.DEFAULT_LANGUAGE_CODE).build();
-        }
-
-        this.mSmartcoin = Smartcoins.getMap().get(locale.getCountry());
+        this.mSmartcoin = Smartcoins.getMap().get(countryCode.toUpperCase());
         HashMap<String, Asset> knownAssets = database.getAssetMap();
         if(!knownAssets.containsKey(this.mSmartcoin.getObjectId())){
             // If the smartcoin asset details are not known, we schedule an update from the full node.
@@ -869,12 +823,9 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             assetList.add(mSmartcoin);
             getMissingAssets = new WebsocketWorkerThread(new LookupAssetSymbols(assetList, mLookupAssetsSymbolsListener));
             getMissingAssets.start();
-            Log.d(TAG,"Don't know much about this smartcoin, making a network query");
         }else{
-            Log.d(TAG, "Already have the details in database, just filling them");
             mSmartcoin = database.fillAssetDetails(mSmartcoin);
         }
-        Log.d(TAG, String.format("Selected smartcoin: %s", mSmartcoin.getSymbol()));
     }
 
 
@@ -883,6 +834,8 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_balances, container, false);
         ButterKnife.bind(this, rootView);
+        language = Helper.fetchStringSharePref(getActivity(), getString(R.string.pref_language), "");
+        locale = new Locale(language);
         balanceActivity = getActivity();
         format = NumberFormat.getInstance(locale);
         tvUpgradeLtm.setPaintFlags(tvUpgradeLtm.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
@@ -1025,16 +978,16 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         isCheckedTimeZone = Helper.fetchBoolianSharePref(getActivity(), getString(R.string.pre_ischecked_timezone));
         Boolean accountNameChange = checkIfAccountNameChange();
 
-        String smartcoinSymbol = mSmartcoin.getSymbol();
-        if (accountNameChange || (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(smartcoinSymbol)))
+        if (accountNameChange || (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency)))
             llBalances.removeAllViews();
 
-        if (isHideDonationsChanged || accountNameChange || (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(mSmartcoin.getSymbol()))) {
-            if (smartcoinSymbol != null && !Helper.getFadeCurrency(getContext()).equals(smartcoinSymbol)) {
+        if (isHideDonationsChanged || accountNameChange || (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency))) {
+            if (finalFaitCurrency != null && !Helper.getFadeCurrency(getContext()).equals(finalFaitCurrency)) {
                 loadBasic(true, accountNameChange, true);
             } else {
                 loadBasic(true, accountNameChange, false);
             }
+
         }
 
         if (!accountId.equals("")) {
@@ -1419,6 +1372,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
         if (this.balanceItems.findBalanceItemBySymbol(assetName) != null){
             this.balanceItems.updateFaitBalanceItem(assetName, value);
         } else {
+            Log.i(TAG, "tvAsset tv Amount tvFaitAmount nulls");
             updateEquivalentAmount.postDelayed(getEquivalentCompRunnable, 500);
         }
     }
@@ -1431,15 +1385,22 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             }
         };
 
+        String faitCurrency = Helper.getFadeCurrency(getContext());
+
+        if (faitCurrency.isEmpty()) {
+            faitCurrency = "EUR";
+        }
+        finalFaitCurrency = faitCurrency;
+
         HashMap<String, ArrayList<String>> currenciesChange = new HashMap();
 
         for (int i = 0; i < accountAssets.size(); i++) {
             AccountAssets accountAsset = accountAssets.get(i);
-            if (!accountAsset.symbol.equals(mSmartcoin.getSymbol())) {
+            if (!accountAsset.symbol.equals(faitCurrency)) {
                 if (!currenciesChange.containsKey(accountAsset.symbol)) {
                     currenciesChange.put(accountAsset.symbol, new ArrayList());
                 }
-                currenciesChange.get(accountAsset.symbol).add(mSmartcoin.getSymbol());
+                currenciesChange.get(accountAsset.symbol).add(faitCurrency);
             }
         }
         this.getEquivalentComponent(currenciesChange, getEquivalentCompRunnable);
@@ -1761,6 +1722,8 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             Long newAmmount = Long.parseLong(newItem.getAmmount());
 
             if (oldAmmount > newAmmount) {
+                Log.d("Balances Update", "Balance sent");
+
                 ammountTextView.setTypeface(ammountTextView.getTypeface(), Typeface.BOLD);
                 ammountTextView.setTextColor(getResources().getColor(R.color.red));
 
@@ -1874,17 +1837,18 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
             //Now, we update the fait (EquivalentComponent)
             if ((newAmmount != 0) && (!newItem.getFait().equals(""))) {
                 try {
-                    final Currency currency = Currency.getInstance(mSmartcoin.getSymbol());
+                    Log.d("Equivalent Value Update", "Changing Fait Text: "+newItem.getSymbol());
+                    final Currency currency = Currency.getInstance(finalFaitCurrency);
                     double d = convertLocalizeStringToDouble(returnFromPower(newItem.getPrecision(), newItem.getAmmount()));
                     final Double eqAmount = d * convertLocalizeStringToDouble(newItem.getFait());
+                    String faitString = "";
+                    if (Helper.isRTL(locale, currency.getSymbol())) {
+                        faitString = String.format(locale, "%.2f %s", eqAmount, currency.getSymbol());
+                    } else {
+                        faitString = String.format(locale, "%s %.2f", currency.getSymbol(), eqAmount);
+                    }
 
-                    NumberFormat currencyFormatter = Helper.newCurrencyFormat(getContext(), currency, locale);
-                    Log.i(TAG, currencyFormatter.format(eqAmount));
-
-                    Log.i(TAG,String.format("smartcoin symbol: %s, fiat symbol: %s", mSmartcoin.getSymbol(), currency.getSymbol()));
-                    String fiatString = String.format(locale, "%s", currencyFormatter.format(eqAmount));
-
-                    faitTextView.setText(fiatString);
+                    faitTextView.setText(faitString);
                     faitTextView.setVisibility(View.VISIBLE);
                 } catch (Exception e) {
                     Log.e(TAG, "Error in updateEquivalentValue : " + e.getMessage());
@@ -2292,7 +2256,7 @@ public class BalancesFragment extends Fragment implements AssetDelegate, ISound,
     }
 
     void loadBasic(boolean onResume, boolean accountNameChanged, boolean faitCurrencyChanged) {
-        Log.d(TAG,"loadBasic");
+
         ArrayList<AccountDetails> accountDetails = tinyDB.getListObject(getString(R.string.pref_wallet_accounts), AccountDetails.class);
         if (accountDetails.size() == 1) {
             accountDetailsId = 0;