9fb93ff9efe6a869a3a3f79f48227fc42364a457
Henry Varona <henry.varona@gmail.com>
2017-02-15 22:07:04 -0400

    - Following Transaction confirms

--- app/src/main/java/de/bitsharesmunich/cryptocoincore/insightapi/InsightApiConstants.java
+++ app/src/main/java/de/bitsharesmunich/cryptocoincore/insightapi/InsightApiConstants.java
@@ -16,6 +16,9 @@ abstract class InsightApiConstants {
     static final String subscribeEmmit = "subscribe";
     static final String txTag = "txi";
 
+    static int MIN_CONFIRM = 6; //minimun number of confirmations of a transaction
+    static long WAIT_TIME = (5 *60 *1000); //wait 5 minutes
+
     static{
         serverAddressPort.put(Coin.BITCOIN,new AddressPort("fr.blockpay.ch",3003));
         serverAddressPort.put(Coin.BITCOIN_TEST,new AddressPort("fr.blockpay.ch",3003));
